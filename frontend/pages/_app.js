1e61fca1dbecc27790041cb8235e1cdca44e207a
Clayton Orman <claytonkfc@Claytons-MacBook-Pro.local>
2020-04-25 13:17:58 -0400

    :art: css and styled components

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,11 @@
 import React from "react";
-import App, { Container } from "next/app";
 import Page from "../components/Page";
 
 const MyApp = ({ Component }) => {
   return (
-    <Container>
-      <Page>
-        <Component />
-      </Page>
-    </Container>
+    <Page>
+      <Component />
+    </Page>
   );
 };
 
